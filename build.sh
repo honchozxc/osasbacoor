619aec725656b59344cfcf0e788c0688bf313971
honchozxc <honchozxc@gmail.com>
2025-11-18 21:01:47 +0800

    Add build script for Render

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# build.sh - Render.com build script
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run database migrations
+python manage.py migrate
