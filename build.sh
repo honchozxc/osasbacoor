91adaac10dca5094505344de3a0afa8de0563c5c
honchozxc <honchozxc@gmail.com>
2025-11-18 21:49:30 +0800

    Fix deployment - separate build and runtime processes

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Run database migrations
-python manage.py migrate
+# Skip migrations during build - they'll run on startup
+echo "Skipping migrations during build (will run on startup)"
