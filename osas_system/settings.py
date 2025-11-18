7e978903e348ffd573503ddfa7a4435f14a5c396
honchozxc <honchozxc@gmail.com>
2025-11-19 00:33:17 +0800

    Add STATIC_ROOT for production

--- osas_system/settings.py
+++ osas_system/settings.py
@@ -91,6 +91,7 @@ LOGOUT_REDIRECT_URL = 'login'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
